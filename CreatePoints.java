05505bd0f5251aaff07a190043c8cdb57960d5ca
JonathanXu4 <59599239+JonathanXu4@users.noreply.github.com>
2022-03-16 21:22:25 -0700

    Finished createPoints file

--- CreatePoints.java
+++ CreatePoints.java
@@ -6,6 +6,9 @@ public class CreatePoints {
       PrintStream out = new PrintStream(new File("points.txt"));
       Random rand = new Random();
       // Random numbers between -50 to positive 50
-      out.println(rand.nextInt(101) - 50);
+      for (int i = 0; i < 100; i++) {
+         out.print("[" + (rand.nextInt(101) - 50) + ", ");
+         out.println(rand.nextInt(101) - 50 + "]");
+      }
    }
 }
