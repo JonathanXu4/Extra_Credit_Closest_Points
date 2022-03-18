ec7eed852f88004d5b51226519d4dc01d3321bdc
JonathanXu4 <59599239+JonathanXu4@users.noreply.github.com>
2022-03-17 18:47:42 -0700

    changed points to be already sorted by x

--- ClosestPoints.java
+++ ClosestPoints.java
@@ -8,20 +8,50 @@ public class ClosestPoints {
       float lap = 0;
       float total = 0;
       System.out.println(lap - start);
-      int[][] hundredPoints = new int[100][2];
+      Point[] hundredPoints = new Point[100];
       for (int i = 0; i < hundredPoints.length; i++) {
-         int[] points = new int[2];
-         points[0] = in.nextInt();
-         points[1] = in.nextInt();
-         hundredPoints[i] = points;
+         hundredPoints[i] = new Point(in.nextInt(), in.nextInt());
+
       }
       // testing print
+      Arrays.sort(hundredPoints);
+      
       for (int i = 0; i < hundredPoints.length; i++) {
-         System.out.println(hundredPoints[i][0] + " " + hundredPoints[i][1]);
+         System.out.println(hundredPoints[i]);
       }
+      start = System.nanoTime();
+      for (int i = 0; i < hundredPoints.length; i++) {
+         for (int j = i; j < hundredPoints.length; j++) {
+            /*System.out.println(distance(hundredPoints[i][0], 
+                                        hundredPoints[i][1],
+                                        hundredPoints[j][0],
+                                        hundredPoints[j][1]));*/
+         }
+      }
+      lap = System.nanoTime();
+      System.out.println();
+      System.out.println(lap - start);
    }
    
    public static double distance(int x1, int y1, int x2, int y2) {
       return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+   }
+}
+
+class Point implements Comparable<Point> {
+   int x;
+   int y;
+   
+   public Point(int x, int y){
+      this.x = x;
+      this.y = y;
+   }
+   
+   public String toString() {
+      return "(" + x + ", " + y + ")";
+   }
+   
+   public int compareTo(Point other) {
+      return y - other.y;
    }
 }
--- CreatePoints.java
+++ CreatePoints.java
@@ -5,9 +5,14 @@ public class CreatePoints {
    public static void main(String[] args) throws FileNotFoundException {
       PrintStream out = new PrintStream(new File("points.txt"));
       Random rand = new Random();
+      int[] x = new int[100];
       // Random numbers between -50 to positive 50
       for (int i = 0; i < 100; i++) {
-         out.println((rand.nextInt(101) - 50) + " " + (rand.nextInt(101) - 50));
+         x[i] = rand.nextInt(101) - 50;
+      }
+      Arrays.sort(x);
+      for (int i = 0; i < 100; i++) {
+         out.println(x[i] + " " + (rand.nextInt(101) - 50));
       }
    }
 }
